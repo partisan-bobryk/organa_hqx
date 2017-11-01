4ebc09f4a1d663024a86c272192b46505707ca9e
7thFox <dev7thFox@gmail.com>
2017-11-01 18:47:16 -0500

    Setup DCCI package

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import cv2
 import numpy as np
+import packages.dcci as dcci
 
 # Init
 window_name = ""
 
+dcci.dcci()
 
 # Exit application
 cv2.waitKey(0)
---
+++ packages/dcci.py
@@ -0,0 +1,3 @@
+
+def dcci():
+    print("Hello, World!")
