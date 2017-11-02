3a73cdea11a9f9dcc7630a49697c0121aa82d7b8
7thFox <dev7thFox@gmail.com>
2017-11-01 20:14:24 -0500

    Added main sub-image loop

--- packages/dcci.py
+++ packages/dcci.py
@@ -1,4 +1,5 @@
 from enum import Enum
+import numpy as np
 
 class DiagClassification(Enum):
     UP_RIGHT   = 1
@@ -11,30 +12,42 @@ class OrthClassification(Enum):
     SMOOTH     = 3
 
 def Dccix2(img):
-    # Iterate over each sub-image, s:
-    
-    diagClass = classifyOrth()
-    if diagClass == DiagClassification.UP_RIGHT:
-        upRight()
-    elif diagClass == DiagClassification.DOWN_RIGHT:
-        downRight()
-    else:
-        diagSmooth()
+    ly, lx = img.shape
 
-    orthClass = classifyOrth()
-    if diagClass == OrthClassification.HORIZONTAL:
-        horizontal()
-    elif diagClass == OrthClassification.VERTICAL:
-        vertical()
-    else:
-        orthSmooth()
+    img_padded = np.zeros((ly+6, lx+6), dtype=np.uint8)
+    img_padded[3:-3,3:-3] = img
+    print(img_padded[-10:, -10:])
+
+
+    img2 = np.zeros((ly*2+12, lx*2+12))
+
+    for x in range(0, lx+3):
+        for y in range(0, ly+3):  
+            s = img_padded[y:y+4, x:x+4]
+            print(s)
+
+            diagClass = classifyOrth(s)
+            if diagClass == DiagClassification.UP_RIGHT:
+                upRight()
+            elif diagClass == DiagClassification.DOWN_RIGHT:
+                downRight()
+            else:
+                diagSmooth()
+
+            orthClass = classifyOrth(s)
+            if diagClass == OrthClassification.HORIZONTAL:
+                horizontal()
+            elif diagClass == OrthClassification.VERTICAL:
+                vertical()
+            else:
+                orthSmooth()
 
     return img
 
-def classifyDiag():
+def classifyDiag(s):
     return DiagClassification.UP_RIGHT
 
-def classifyOrth():
+def classifyOrth(s):
     return OrthClassification.HORIZONTAL
 
 def upRight():
