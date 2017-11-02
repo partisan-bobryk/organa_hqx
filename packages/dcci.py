fe59563bfe969d448de64ef5700e293e9102fe9e
7thFox <dev7thFox@gmail.com>
2017-11-01 20:38:56 -0500

    added pixel replacement logic: all set for sub-function implementations

--- packages/dcci.py
+++ packages/dcci.py
@@ -14,35 +14,45 @@ class OrthClassification(Enum):
 def Dccix2(img):
     ly, lx = img.shape
 
-    img_padded = np.zeros((ly+6, lx+6), dtype=np.uint8)
-    img_padded[3:-3,3:-3] = img
-    print(img_padded[-10:, -10:])
+    imgPadded = np.zeros((ly+6, lx+6), dtype=np.uint8)
+    imgPadded[3:-3,3:-3] = img
 
-
-    img2 = np.zeros((ly*2+12, lx*2+12))
+    imgOutPadded = np.zeros((ly*2+12, lx*2+12))
 
     for x in range(0, lx+3):
         for y in range(0, ly+3):  
-            s = img_padded[y:y+4, x:x+4]
-            print(s)
+            s = imgPadded[y:y+4, x:x+4]
+
+            # Add Original Pixels
+            imgOutPadded[2*y:2*y+7:2, 2*x:2*x+7:2] += s
 
             diagClass = classifyOrth(s)
+            diag = None
             if diagClass == DiagClassification.UP_RIGHT:
-                upRight()
+                diag = upRight(s)
             elif diagClass == DiagClassification.DOWN_RIGHT:
-                downRight()
+                diag = downRight(s)
             else:
-                diagSmooth()
+                diag = diagSmooth(s)
 
+            # Add diagonal pixels to both the output and replace the pixels in s
+            imgOutPadded[2*y+1:2*y+1+5:2,2*x+1:2*x+1+5:2] += diag[1:6:2, 1:6:2]
+            s[1:6:2, 1:6:2] = s[1:6:2, 1:6:2]
+
+            orth = None
             orthClass = classifyOrth(s)
             if diagClass == OrthClassification.HORIZONTAL:
-                horizontal()
+                orth = horizontal(s)
             elif diagClass == OrthClassification.VERTICAL:
-                vertical()
+                orth = vertical(s)
             else:
-                orthSmooth()
+                orth = orthSmooth(s)
 
-    return img
+            # Replace orthogonal pixels
+            imgOutPadded[2*y:2*y+7:2, 2*x+1:2*x+1+5:2] += orth[0:7:2, 1:6:2]
+            imgOutPadded[2*y+1:2*y+1+5:2, 2*x:2*x+7:2] += orth[1:6:2, 0:7:2]
+
+    return np.uint8(np.round(imgOutPadded[6:-6, 6:-6]/16))
 
 def classifyDiag(s):
     return DiagClassification.UP_RIGHT
@@ -50,20 +60,20 @@ def classifyDiag(s):
 def classifyOrth(s):
     return OrthClassification.HORIZONTAL
 
-def upRight():
-    return
+def upRight(s):
+    return np.zeros((7,7)) + 255
 
-def downRight():
-    return
+def downRight(s):
+    return np.zeros((7,7)) + 255
 
-def diagSmooth():
-    return
+def diagSmooth(s):
+    return np.zeros((7,7)) + 255
 
-def horizontal():
-    return
+def horizontal(s):
+    return np.zeros((7,7)) + 255
 
-def vertical():
-    return
+def vertical(s):
+    return np.zeros((7,7)) + 255
 
-def orthSmooth():
-    return
+def orthSmooth(s):
+    return np.zeros((7,7)) + 255
