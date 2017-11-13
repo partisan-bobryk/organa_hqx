c47d3d020a5671755987e2ed67d5e3cd281f5b86
Justin Ellefson <ellefsonj0224@my.uwstout.edu>
2017-11-13 11:21:22 -0600

    Fixes to diagonal calculations

--- packages/dcci.py
+++ packages/dcci.py
@@ -82,9 +82,9 @@ def classifyDiag(s):
     d1 = np.sum(np.abs(s[1:,:-1] - s[:-1,1:]))
     d2 = np.sum(np.abs(s[1:,1:] - s[:-1,:-1]))
 
-    if (1+d1) > 115 * (1+d2):
+    if (100*(1+d1) > 115 * (1+d2)):
         return DiagClassification.UP_RIGHT, d1, d2
-    elif (1+d2) > 115 * (1+d1):
+    elif (100*(1+d2) > 115 * (1+d1)):
         return DiagClassification.DOWN_RIGHT, d1, d2
     else:
         return DiagClassification.SMOOTH, d1, d2
@@ -96,9 +96,9 @@ def classifyOrth(s2):
     d2 = np.sum(np.abs(s2[-2:, 1:] - s2[-2:, -1:]))
 
     # Avoiding floating point error
-    if  (1 + d1) > 115 * (1 + d2):
+    if  (100 * (1 + d1) > 115 * (1 + d2)):
         return OrthClassification.HORIZONTAL, d1, d2
-    elif (1 + d2) > 115 * (1 + d1):
+    elif ( 100 * (1 + d2) > 115 * (1 + d1)):
         return OrthClassification.VERTICAL, d1, d2
     else:
         return OrthClassification.SMOOTH, d1, d2
@@ -109,53 +109,64 @@ def classifyOrth(s2):
 # Output: 7x7 interpolated area (Only diagonals used)
 def upRight(s):
     #(-1 * P(0, 0) + 9 * P(1, 1) + 9 * P(2, 2) - 1 * P(3, 3)) / 16
-    op = (-1. * s[0,0] + 9. * s[1,1] + (-1.0)*s[3,3]) / 16.0
+    op = (-1 * s[0,0] + 9 * s[1,1] + 9 * s[2,2] + (-1)*s[3,3]) / 16
+    op = 255 if op > 255 else op
+    op = 0 if op < 0 else op
     return op
 
 # Input: 4x4 area
 # Output: 7x7 interpolated area (Only diagonals used)
 def downRight(s):
     #Output pixel = (-1 * P(3, 0) + 9 * P(2, 1) + 9 * P(1, 2) - 1 * P(0, 3)) / 16
-    op = ((-1.)*s[0,3] + 9. * s[2,1] + (-1.)*s[3,0] ) / 16.0
+    op = ((-1)*s[3,0] + 9 * s[1,2] + 9* s[2,1] + (-1)*s[0,3] ) / 16
+    op = 255 if op > 255 else op
+    op = 0 if op < 0 else op
     return op
 
 # Input: 4x4 area
 # Output: 7x7 interpolated area (Only diagonals used)
 def diagSmooth(s,d1,d2):
-    w1 = 1.0 / (1.0 + d1**5)
-    w2 = 1.0 / (1.0 + d2**5.0)
+    w1 = 1 / (1 + d1**5)
+    w2 = 1 / (1 + d2**5)
     weight1 = w1 / (w1 + w2)
     weight2 = w2 / (w1 + w2)
 
-    downRightPixel = (-1 * s[0,0] + 9 * s[1, 1] + 9. * s[2, 2] - 1 * s[3, 3]) / 16.
-    upRightPixel = (-1. * s[0, 3] + 9. * s[1, 2] + 9. * s[2, 1] - 1. * s[3, 0]) / 16.
+    downRightPixel = (-1 * s[0,0] + 9 * s[1, 1] + 9 * s[2, 2] - 1 * s[3, 3]) / 16
+    upRightPixel = (-1 * s[3,0] + 9 * s[2,1] + 9 * s[1,2] - 1 * s[0,3]) / 16
 
     op = downRightPixel * weight1 + upRightPixel * weight2
-
+    op = 255 if op > 255 else op
+    op = 0 if op < 0 else op
     return op
 
 # Input: 7x7 padded "diamond" area
 # Output: 7x7 interpolated area (Only orthogonals used)
 def horizontal(s, x=3, y=3):
-    return (-1 * s[x, y-3] + 9 * s[x, y-1] + 9 * s[x, y+1] - 1 * s[x, y + 3]) / 16
-
+    op = (-1 * s[x, y-3] + 9 * s[x, y-1] + 9 * s[x, y+1] - 1 * s[x, y + 3]) / 16
+    op = 255 if op > 255 else op
+    op = 0 if op < 0 else op
+    return op
 # Input: 7x7 padded "diamond" area
 # Output: 7x7 interpolated area (Only orthogonals used)
 def vertical(s, x=3, y=3):
-    return (-1 * s[x-3, y] + 9 * s[x - 1, y] + 9 * s[x + 1, y] - 1 * s[x + 3, y]) / 16
+    op = (-1 * s[x-3, y] + 9 * s[x - 1, y] + 9 * s[x + 1, y] - 1 * s[x + 3, y]) / 16
+    op = 255 if op > 255 else op
+    op = 0 if op < 0 else op
+    return op
 
 # Input: 7x7 padded "diamond" area
 # Output: 7x7 interpolated area (Only orthogonals used)
 def orthSmooth(s, d1, d2):
     x,y = 3, 3
-    w1 = 1.0 / (1.0 + d1 ** 5.0)
-    w2 = 1.0 / (1.0 + d2 ** 5.0)
+    w1 = 1 / (1 + d1 ** 5)
+    w2 = 1 / (1 + d2 ** 5)
     weight1 = w1 / (w1 + w2)
     weight2 = w2 / (w1 + w2)
 
-    horizontalPixel = (-1 * s[x-3, y] + 9 * s[x-1, y] + 9. * s[x+1, y] - 1 * s[x+3, y]) / 16.
-    veritcalPixel = (-1. * s[x, y-3] + 9. * s[x, y-1] + 9. * s[x, y+1] - 1. * s[x, y+3]) / 16.
+    horizontalPixel = (-1 * s[x-3, y] + 9 * s[x-1, y] + 9 * s[x+1, y] - 1 * s[x+3, y]) / 16
+    veritcalPixel = (-1 * s[x, y-3] + 9 * s[x, y-1] + 9 * s[x, y+1] - 1 * s[x, y+3]) / 16
 
     op = horizontalPixel * weight1 + veritcalPixel * weight2
-
+    op = 255 if op > 255 else op
+    op = 0 if op < 0 else op
     return op
