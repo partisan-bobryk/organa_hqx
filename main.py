b0f8e5572b13777054fc4fe155bd8328219551c1
Maksym Yadvinskyy <maksym.yadvinskyy@gmail.com>
2017-11-13 17:47:51 -0600

    Added resource file

--- main.py
+++ main.py
@@ -1,34 +1,10 @@
 import cv2
 import numpy as np
 import packages.dcci as dcci
+from img_resources import files
 
 # Init
 window_name = "UpScaling"
-files = [
-    "./resources/bonzai.tif",
-    "./resources/cameraman.tif",
-    "./resources/diagonal_left_100x100.tif",
-    "./resources/grayscale_test_1.png",
-    "./resources/house.tif",
-    "./resources/jetplane.tif",
-    "./resources/lake.tif",
-    # "./resources/lena_color_256.tif",
-    # "./resources/lena_color_512.tif",
-    "./resources/lena_gray_256.tif",
-    # "./resources/lena_gray_512.tif",
-    "./resources/link.png",
-    "./resources/livingroom.tif",
-    # "./resources/mandril_color.tif",
-    "./resources/mandril_gray.tif",
-    "./resources/multi_grayscale_1_100x100.tif",
-    # "./resources/peppers_color.tif",
-    "./resources/peppers_gray.tif",
-    "./resources/pirate.tif",
-    "./resources/plus_100x100.tif",
-    "./resources/walkbridge.tif",
-    "./resources/woman_blonde.tif",
-    "./resources/woman_darkhair.tif",
-]
 
 for file in files:
     img = cv2.imread(file, cv2.IMREAD_GRAYSCALE)
@@ -38,11 +14,8 @@ for file in files:
     img2[img2 < 0] = 0
     img2 = np.uint8(np.round(img2))
 
-    bicubic = cv2.resize(img[::2,::2],(0,0), fx=4., fy=4., interpolation=cv2.INTER_CUBIC)
-
     print(img2.shape)
-    print(bicubic.shape)
 
-    cv2.imshow(window_name, np.concatenate((bicubic[:-3,:-3],img2), axis=1))
+    cv2.imshow(window_name, img2)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
