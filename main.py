165b369c573b5a39f1d41755666175bc4252a2f3
7thFox <dev7thFox@gmail.com>
2017-11-15 17:59:41 -0600

    Added variable T and k values and updated main.py to work with current resource setup and to display a grid of T/k options

--- main.py
+++ main.py
@@ -1,21 +1,41 @@
 import cv2
 import numpy as np
 import packages.dcci as dcci
-from img_resources import files
+import img_resources as imr
 
 # Init
 window_name = "UpScaling"
 
-for file in files:
-    img = cv2.imread(file, cv2.IMREAD_GRAYSCALE)
-    img2 = dcci.Dccix2(np.float64(img[::2, ::2]))
-    img2 = dcci.Dccix2(img2)
-    img2[img2 > 255] = 255
-    img2[img2 < 0] = 0
-    img2 = np.uint8(np.round(img2))
+N = 2 # 2^N scaling
+# Ts, ks = range(100,151,5), range(1,7,1)# Used for testing T/k values
+Ts, ks = [115], [5] # Default T/k's; use for single/fast image
 
-    print(img2.shape)
+for file in imr.rpg_items:
+    img = cv2.imread(file, cv2.IMREAD_COLOR)
+    img2=np.array([])
+    for k in ks:
+        img3 = np.array([])
+        for T in Ts:
+            img2b = dcci.Dccix2(img[:,:,0], T, k)
+            img2g = dcci.Dccix2(img[:,:,1], T, k)
+            img2r = dcci.Dccix2(img[:,:,2], T, k)
 
-    cv2.imshow(window_name, img2)
+            for i in range(1,N):
+                img2b = dcci.Dccix2(img2b, T, k)
+                img2g = dcci.Dccix2(img2g, T, k)
+                img2r = dcci.Dccix2(img2r, T, k)
+            
+            img4 = np.stack((img2b,img2g,img2r),axis=2)
+            if img3.shape[0] == 0:
+                img3 = img4
+            else:
+                img3 = np.concatenate((img3,img4), axis=1)
+        
+        if img2.shape[0] == 0:
+            img2 = img3
+        else:
+            img2 = np.concatenate((img2,img3), axis=0)
+
+    cv2.imshow(f"{window_name} - {file}", img2)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
