cfca476e2ca8770a508c2cc18d41af84c1f2bfee
7thFox <dev7thFox@gmail.com>
2017-11-15 16:32:22 -0600

    Fixed file names

--- test.py
+++ test.py
@@ -11,21 +11,23 @@ lenczos_time = time.time()
 # Init
 window_name = "UpScaling"
 
-img = cv2.imread(imr.all[0], cv2.IMREAD_GRAYSCALE)[::2, ::2]
-img2 = dcci.Dccix2(img)
-print('DCCI took {}'.format(dcci_time - time.time()))
+for file in imr.rpg_items:
+    print(file)
+    img = cv2.imread(file, cv2.IMREAD_GRAYSCALE)[::2, ::2]
+    img2 = dcci.Dccix2(img)
+    print('DCCI took {}'.format(dcci_time - time.time()))
 
-bicubic = cv2.resize(img, (0,0), fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
-print('Bicubic took {}'.format(bicubic_time - time.time()))
-lenczos = cv2.resize(img, (0,0), fx=2, fy=2, interpolation=cv2.INTER_LANCZOS4)
-print('Lanczos took {}'.format(lenczos_time - time.time()))
+    bicubic = cv2.resize(img, (0,0), fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
+    print('Bicubic took {}'.format(bicubic_time - time.time()))
+    lenczos = cv2.resize(img, (0,0), fx=2, fy=2, interpolation=cv2.INTER_LANCZOS4)
+    print('Lanczos took {}'.format(lenczos_time - time.time()))
 
-cv2.imshow(window_name, img)
-cv2.imshow(window_name + " DCCI ", np.uint8(np.round(img2)))
+    cv2.imshow(window_name, img)
+    cv2.imshow(window_name + " DCCI ", np.uint8(np.round(img2)))
 
-cv2.imshow(window_name + " Bicubic ", bicubic)
-cv2.imshow(window_name + " Lenczos ", lenczos)
+    cv2.imshow(window_name + " Bicubic ", bicubic)
+    cv2.imshow(window_name + " Lenczos ", lenczos)
 
-# Exit application
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    # Exit application
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
