0651ce0bd12e84fc1956ff557d7de7cccc9ce0e3
Maksym Yadvinskyy <maksym.yadvinskyy@gmail.com>
2017-11-15 18:57:07 -0600

    Modified test

--- test.py
+++ test.py
@@ -2,31 +2,33 @@ import cv2
 import numpy as np
 import time
 import packages.dcci as dcci
-import img_resources as imr
-
-dcci_time = time.time()
-bicubic_time = time.time()
-lenczos_time = time.time()
+import img_resources as img_files
 
 # Init
 window_name = "UpScaling"
 
-for file in imr.rpg_items:
-    img = cv2.imread(file, cv2.IMREAD_GRAYSCALE)
-    img2 = dcci.Dccix2(img)
-    print('DCCI took {}'.format(dcci_time - time.time()))
+img = cv2.imread(img_files.pixel_art[0], cv2.IMREAD_GRAYSCALE)
 
-    bicubic = cv2.resize(img, (0,0), fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
-    print('Bicubic took {}'.format(bicubic_time - time.time()))
-    lenczos = cv2.resize(img, (0,0), fx=2, fy=2, interpolation=cv2.INTER_LANCZOS4)
-    print('Lanczos took {}'.format(lenczos_time - time.time()))
+def time_results(fn):
+    time_start = time.clock()
+    output = fn
+    time_stop = time.clock()
+    time_passed = time_stop - time_start
+    return output, time_passed
 
-    cv2.imshow(window_name, img)
-    cv2.imshow(window_name + " DCCI ", np.uint8(np.round(img2)))
+dcci_img, dcci_time = time_results(dcci.Dccix2(np.float64(img)))
+bicubic_img, bicubic_time = time_results(cv2.resize(img, (0,0), fx=2, fy=2, interpolation=cv2.INTER_CUBIC))
+lenczos_img, lenczos_time = time_results(cv2.resize(img, (0,0), fx=2, fy=2, interpolation=cv2.INTER_LANCZOS4))
 
-    cv2.imshow(window_name + " Bicubic ", bicubic)
-    cv2.imshow(window_name + " Lenczos ", lenczos)
+# Return time results
+print('DCCI took {}'.format(dcci_time))
+print('Bicubic took {}'.format(bicubic_time))
+print('lenczos took {}'.format(lenczos_time))
 
-    # Exit application
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+cv2.imshow("Original TEST", img)
+cv2.imshow("DCCI TEST", dcci_img)
+cv2.imshow("Bicubic TEST", bicubic_img)
+cv2.imshow("lenczos TEST", lenczos_img)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
