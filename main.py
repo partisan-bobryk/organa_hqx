737e44f0b53e8128e8fc8c951cc3671c85a22248
7thFox <dev7thFox@gmail.com>
2017-12-11 10:55:48 -0600

    Cleaned up DCCI implementation

--- main.py
+++ main.py
@@ -6,56 +6,40 @@ import img_resources as imr
 # Init
 window_name = "UpScaling"
 
-N = 3 # 2^N scaling
-pad = 4
+N = 1 # 2^N scaling
+
+# timg = np.zeros((7,7), dtype=np.uint8) +255
+# timg[2:-2:2,2:-2:2] = 0
+
+# timg = np.array([[255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255],
+#                  [255, 255, 255, 255,   0, 255,   0, 255, 255, 255, 255],
+#                  [255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255],
+#                  [255, 255, 255,   0, 255,   0, 255,   0, 255, 255, 255],
+#                  [255,   0, 255, 255, 255,   0, 255, 255, 255,   0, 255],
+#                  [255, 255, 255,   0,   0,   0,   0,   0, 255, 255, 255],
+#                  [255,   0, 255, 255, 255,   0, 255, 255, 255,   0, 255],
+#                  [255, 255, 255,   0, 255,   0, 255,   0, 255, 255, 255],
+#                  [255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255],
+#                  [255, 255, 255, 255,   0, 255,   0, 255, 255, 255, 255],
+#                  [255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255]], dtype=np.uint8)
+
+# for imgT in [timg]:
+#     cv2.imshow(f"{window_name}", dcci.Dcci(imgT))
+#     cv2.waitKey(0)
+#     cv2.destroyAllWindows()
 
 
-# f = np.zeros((32,32,3), dtype=np.uint8)
-
-# f += 64
-# # f *= 2
-# cv2.imwrite("resources/stripes.png", f)
-
-
-# img = np.array(range(0,4))**2
-# img = [img]
-# # img = np.matmul(img,img.transpose())
-# img = np.matmul(np.transpose(img),img)
-# print(f"Original:\n{img}")
-
-# imgO = dcci.Dccix2(img)
-
-# print(f"Output:\n{imgO}")
-
-for file in imr.rpg_items_sheet:
+for file in imr.test_images[7:8]:
     print(file)
     imgT = cv2.imread(file, cv2.IMREAD_COLOR)
-
-    y,x,z = imgT.shape
-    img = np.zeros((y+2*pad, x+2*pad,z), dtype=np.uint8)
-    img[pad:-pad,pad:-pad] = imgT
-
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2Lab)
-
-    for n in range(0,N):
-        imgB = dcci.Dccix2(img[:,:,0])
-        imgG = dcci.Dccix2(img[:,:,1])
-        imgR = dcci.Dccix2(img[:,:,2])
-        img = np.stack((imgB, imgG, imgR), axis=2)
-
-    img = img[(2**N)*pad:-(2**N)*pad,(2**N)*pad:-(2**N)*pad]
-    img = cv2.cvtColor(img, cv2.COLOR_Lab2BGR)
-
+    img = dcci.Dcci(imgT,N)
 
     img2 = cv2.resize(imgT, (0,0), fx=2**N, fy=2**N, interpolation=cv2.INTER_CUBIC)
     img2 = img2[:-(2**N-1),:-(2**N-1)]
+
     b = np.uint8(np.zeros((img.shape[0],4,3)) + (0,0,255))
 
-
     imgOut = np.concatenate((img,b,img2), axis=1)
-    cv2.imwrite("./output/latest.png", imgOut)
-    # cv2.imwrite("./output/latest-bicubic.png", img2)
-        # cv2.imshow(f"{window_name} - {file}2", img2)
     cv2.imshow(f"{window_name} (DCCI : Bicubic)- {file}", imgOut)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
--- packages/dcci.py
+++ packages/dcci.py
@@ -12,14 +12,42 @@ class OrthClassification(Enum):
     VERTICAL   = 2
     SMOOTH     = 3
 
-def Dccix2(img, T=115, k=5):
+def isColor(img):
+    return len(img.shape) == 3
+
+def Dcci(img, N=1, T=155, k=5):
+    COLOR_PAD = 4
+    if isColor(img):
+        y,x,z = img.shape
+        imgP = np.zeros((y+2*COLOR_PAD, x+2*COLOR_PAD,z), dtype=np.uint8)
+        imgP[COLOR_PAD:-COLOR_PAD,COLOR_PAD:-COLOR_PAD] = img
+        img = cv2.cvtColor(imgP, cv2.COLOR_BGR2Lab)
+
+        imgB = dcciN(img[:,:,0], N, T, k)
+        imgG = dcciN(img[:,:,1], N, T, k)
+        imgR = dcciN(img[:,:,2], N, T, k)
+        img = np.stack((imgB, imgG, imgR), axis=2)
+
+        img = img[(2**N)*COLOR_PAD:-(2**N)*COLOR_PAD,(2**N)*COLOR_PAD:-(2**N)*COLOR_PAD]
+        img = cv2.cvtColor(img, cv2.COLOR_Lab2BGR)
+        return img
+    
+    return dcciN(img, N, T, k)
+
+
+def dcciN(img, N, T, k):
+    for i in range(0,N):
+        img = dccix2(img,T,k)
+    return img
+
+def dccix2(img, T, k):
     img2 = np.float64(np.swapaxes(img, 0, 1))
 
     lx, ly = img2.shape
     imgInterp = np.zeros((lx*2-1, ly*2-1))
     imgInterp[::2,::2] = img2
 
-    imgInterp = interpDiag(img, imgInterp, T, k)
+    imgInterp = interpDiag(img2, imgInterp, T, k)
     imgInterp = interpOrth(imgInterp, T, k)
 
     imgInterp[imgInterp < 0] = 0
@@ -28,9 +56,11 @@ def Dccix2(img, T=115, k=5):
     
     return np.swapaxes(imgInterp, 0, 1)
 
-# Input: The 2x image with black space padding each of the given pixels
-# Output: The same image with the diagonal non-edge pixels interpolated
 def interpDiag(original,img, T, k):
+    """
+    Input:  The 2x image with black space padding each of the given pixels
+    Output: The same image with the diagonal non-edge pixels interpolated
+    """
     lx, ly = img.shape
     imgPadded = np.zeros((lx+4,ly+4))# Pad by 1 given pixel (2 real pixels) on each side
     imgPadded[2:-2,2:-2] = img
@@ -40,82 +70,76 @@ def interpDiag(original,img, T, k):
     # the pixels diagonal from each 4 surrounding pixels of the original image)
     d1 = np.abs(original[1:,:-1] - original[:-1,1:])
     d2 = np.abs(original[1:,1:] - original[:-1,:-1])
-    d1 = cv2.copyMakeBorder(d1, 1,0,1,0, cv2.BORDER_CONSTANT,value=0)
-    d2 = cv2.copyMakeBorder(d2, 1,0,1,0, cv2.BORDER_CONSTANT,value=0)
-
-    # print(original)
-    # print(d2)
-
-    # d1 = cv2.copyMakeBorder(d1, 2,2,2,2, cv2.BORDER_CONSTANT,value=0)
-    # d2 = cv2.copyMakeBorder(d2, 2,2,2,2, cv2.BORDER_CONSTANT,value=0)
+    d1 = cv2.copyMakeBorder(d1, 1,1,1,1, cv2.BORDER_CONSTANT,value=0)
+    d2 = cv2.copyMakeBorder(d2, 1,1,1,1, cv2.BORDER_CONSTANT,value=0)
 
     # Center at the point to be interpolated, (x,y)
     for x in range(3, lx+1, 2):
-        for y in range(3, ly+1, 2):  
+        for y in range(3, ly+1, 2): 
+            rx, ry = x-2, y-2
+            dx, dy = (rx+1)//2, (ry+1)//2
             s4x4 = imgPadded[x-3:x+4:2,y-3:y+4:2] # 4x4 of the original image
-            d1s = np.sum(d1[(x-3)//2-1:(x-3)//2+2, (y-3)//2-1:(y-3)//2+2]) # 3x3 region of differences around x,y
-            d2s = np.sum(d2[(x-3)//2-1:(x-3)//2+2, (y-3)//2-1:(y-3)//2+2])
+            d1k = d1[dx-1:dx+2, dy-1:dy+2] # 3x3 region of differences around x,y
+            d2k = d2[dx-1:dx+2, dy-1:dy+2]
 
-            # print(f"({(x-3)//2},{(y-3)//2}): {d1[(x-3)//2-1:(x-3)//2+2, (y-3)//2-1:(y-3)//2+2]}")
+            d1s = np.sum(d1k)
+            d2s = np.sum(d2k)
 
-            # if x >= 3+0 and x < 3+3 and y >= 3+0 and y < 3+3:
-                # print(f"({x-3},{y-3}): {d1s} : {d2s}")
-                # print(original[x-1:x+2, y-1:y+2])
-                # print(s4x4)
-                # print(d1[x-1:x+2, y-1:y+2])
-                # print(d2[x-1:x+2, y-1:y+2])
-
-            # Get classification
             diagClass = DiagClassification.SMOOTH
             if 100*(1+d1s) > T * (1+d2s):
                 diagClass = DiagClassification.UP_RIGHT
             elif 100*(1+d2s) > T * (1+d1s):
                 diagClass = DiagClassification.DOWN_RIGHT
 
-            
-            # imgPadded[x,y] = upRight(s4x4)
-            # imgPadded[x,y] = downRight(s4x4)
-            # imgPadded[x,y] = diagSmooth(s4x4, d1s, d2s, k)
-
             if diagClass == DiagClassification.UP_RIGHT:
                 imgPadded[x,y] = upRight(s4x4)
-                # print(f"{(x-3)//2}, {(y-3)//2} up")
             elif diagClass == DiagClassification.DOWN_RIGHT:
-                # print(f"{(x-3)//2}, {(y-3)//2} down")
                 imgPadded[x,y] = downRight(s4x4)
             else:
-                # print(f"{(x-3)//2}, {(y-3)//2} smooth")
                 imgPadded[x,y] = diagSmooth(s4x4, d1s, d2s, k)
                 
+
     return imgPadded[2:-2,2:-2]
 
 def interpOrth(img, T, k):
+    PAD = 4
     lx, ly = img.shape
-    imgPadded = np.zeros((lx+6,ly+6))
-    imgPadded[3:-3,3:-3] = img
+    imgPadded = np.zeros((lx+2*PAD,ly+2*PAD))
+    imgPadded[PAD:-PAD,PAD:-PAD] = img
     
-
     # Each (x,y) is the uninterpolated pixel between the diff. It works out that
     # The [unused] diffs surrounding original or diagonally interpolated pixels
     # is equal to 0
-    d1 = np.abs(imgPadded[2:-4,3:-3] - imgPadded[4:-2,3:-3])
-    d2 = np.abs(imgPadded[3:-3, 2:-4] - imgPadded[3:-3, 4:-2])
+    d1 = np.abs(imgPadded[(PAD-1):-(PAD+1),PAD:-PAD] - imgPadded[(PAD+1):-(PAD-1),PAD:-PAD])
+    d2 = np.abs(imgPadded[PAD:-PAD, (PAD-1):-(PAD+1)] - imgPadded[PAD:-PAD, (PAD+1):-(PAD-1)])
 
-    d1 = cv2.copyMakeBorder(d1, 3,3,3,3, cv2.BORDER_CONSTANT,value=0)
-    d2 = cv2.copyMakeBorder(d2, 3,3,3,3, cv2.BORDER_CONSTANT,value=0)
+    d1 = cv2.copyMakeBorder(d1, PAD,PAD,PAD,PAD, cv2.BORDER_CONSTANT,value=0)
+    d2 = cv2.copyMakeBorder(d2, PAD,PAD,PAD,PAD, cv2.BORDER_CONSTANT,value=0)
 
-    # print(f"\nimg:\n{img}")
-    # print(f"d1:\n{d1}")
-    # print(f"d2:\n{d2}\n")
+    #    First Loop
+    #   o x o x o x o
+    #   . o . o . o .
+    #   o x o x o x o
+    #   . o . o . o .
+    #   o x o x o x o
+    #   . o . o . o .
+    #   o x o x o x o
+    #
+    #    2nd Loop
+    #   o . o . o . o
+    #   x o x o x o .
+    #   o . o . o . o
+    #   x o x o x o .
+    #   o . o . o . o
+    #   x o x o x o .
+    #   o . o . o . o
 
     # Center at the point to be interpolated, (x,y), as well as (x-1, y+1)
-    for x in range(4, lx+3, 2):
-        for y in range(3, ly+3, 2): 
+    for x in range(PAD+1, lx+PAD, 2):
+        for y in range(PAD, ly+PAD, 2): 
             s7x7 = imgPadded[x-3:x+4,y-3:y+4]
-
             d1s = d1[x,y] + d1[x+2,y] + d1[x-2,y] + d1[x,y+2] + d1[x,y-2]
             d2s = d2[x,y] + d2[x+2,y] + d2[x-2,y] + d2[x,y+2] + d2[x,y-2]
-
 
             # Match classification and interpolate for x,y
             orthClass = OrthClassification.SMOOTH
@@ -131,8 +155,8 @@ def interpOrth(img, T, k):
             else:
                 imgPadded[x,y] = orthSmooth(s7x7, d1s, d2s, k)
 
-    for x in range(3, lx+3, 2):
-        for y in range(4, ly+3, 2): 
+    for x in range(PAD, lx+PAD, 2):
+        for y in range(PAD+1, ly+PAD, 2): 
             s7x7 = imgPadded[x-3:x+4,y-3:y+4]
             d1s = d1[x,y] + d1[x+2,y] + d1[x-2,y] + d1[x,y+2] + d1[x,y-2]
             d2s = d2[x,y] + d2[x+2,y] + d2[x-2,y] + d2[x,y+2] + d2[x,y-2]
@@ -150,74 +174,71 @@ def interpOrth(img, T, k):
             else:
                 imgPadded[x,y] = orthSmooth(s7x7, d1s, d2s, k)
 
-    return imgPadded[3:-3,3:-3]
+    return imgPadded[PAD:-PAD,PAD:-PAD]
 
-# Input: 7x7 padded "diamond" area
-# Output: Classification
-def classifyOrth(s2, T):
-
-    # Avoiding floating point error
-    if  (100 * (1 + d1) > T * (1 + d2)):
-        return OrthClassification.HORIZONTAL
-    elif ( 100 * (1 + d2) > T * (1 + d1)):
-        return OrthClassification.VERTICAL
-    else:
-        return 
-
-
-
-# Input: 4x4 area
-# Output: 7x7 interpolated area (Only diagonals used)
 def upRight(s):
-    #(-1 * P(0, 0) + 9 * P(1, 1) + 9 * P(2, 2) - 1 * P(3, 3)) / 16
+    """
+    Input:  4x4 area
+    Output: 7x7 interpolated area (Only diagonals used)
+    """
     op = (-1 * s[0,0] + 9 * s[1,1] + 9 * s[2,2] + (-1)*s[3,3]) / 16
     op = 255 if op > 255 else op
     op = 0 if op < 0 else op
     return op
 
-# Input: 4x4 area
-# Output: 7x7 interpolated area (Only diagonals used)
 def downRight(s):
-    #Output pixel = (-1 * P(3, 0) + 9 * P(2, 1) + 9 * P(1, 2) - 1 * P(0, 3)) / 16
+    """
+    Input:  4x4 area
+    Output: 7x7 interpolated area (Only diagonals used)
+    """
     op = ((-1)*s[3,0] + 9 * s[1,2] + 9* s[2,1] + (-1)*s[0,3] ) / 16
     op = 255 if op > 255 else op
     op = 0 if op < 0 else op
     return op
 
-# Input: 4x4 area
-# Output: 7x7 interpolated area (Only diagonals used)
 def diagSmooth(s,d1,d2, k):
+    """
+    Input:  4x4 area
+    Output: 7x7 interpolated area (Only diagonals used)
+    """
     w1 = 1 / (1 + d1**k)
     w2 = 1 / (1 + d2**k)
     weight1 = w1 / (w1 + w2)
     weight2 = w2 / (w1 + w2)
 
-    downRightPixel = (-1 * s[0,0] + 9 * s[1, 1] + 9 * s[2, 2] - 1 * s[3, 3]) / 16
-    upRightPixel = (-1 * s[3,0] + 9 * s[2,1] + 9 * s[1,2] - 1 * s[0,3]) / 16
+    downRightPixel = (-1 * s[0,0] + 9 * s[1,1] + 9 * s[2,2] - 1 * s[3,3]) / 16
+    upRightPixel   = (-1 * s[3,0] + 9 * s[2,1] + 9 * s[1,2] - 1 * s[0,3]) / 16
 
     op = downRightPixel * weight1 + upRightPixel * weight2
     op = 255 if op > 255 else op
     op = 0 if op < 0 else op
     return op
 
-# Input: 7x7 padded "diamond" area
-# Output: 7x7 interpolated area (Only orthogonals used)
 def horizontal(s, x=3, y=3):
+    """
+    Input:  7x7 padded "diamond" area
+    Output: 7x7 interpolated area (Only orthogonals used)
+    """
     op = (-1 * s[x, y-3] + 9 * s[x, y-1] + 9 * s[x, y+1] - 1 * s[x, y + 3]) / 16
     op = 255 if op > 255 else op
     op = 0 if op < 0 else op
     return op
-# Input: 7x7 padded "diamond" area
-# Output: 7x7 interpolated area (Only orthogonals used)
+
 def vertical(s, x=3, y=3):
+    """
+    Input:  7x7 padded "diamond" area
+    Output: 7x7 interpolated area (Only orthogonals used)
+    """
     op = (-1 * s[x-3, y] + 9 * s[x - 1, y] + 9 * s[x + 1, y] - 1 * s[x + 3, y]) / 16
     op = 255 if op > 255 else op
     op = 0 if op < 0 else op
     return op
 
-# Input: 7x7 padded "diamond" area
-# Output: 7x7 interpolated area (Only orthogonals used)
 def orthSmooth(s, d1, d2, k):
+    """
+    Input:  7x7 padded "diamond" area
+    Output: 7x7 interpolated area (Only orthogonals used)
+    """
     x,y = 3, 3
     w1 = 1 / (1 + d1 ** k)
     w2 = 1 / (1 + d2 ** k)
@@ -225,7 +246,7 @@ def orthSmooth(s, d1, d2, k):
     weight2 = w2 / (w1 + w2)
 
     horizontalPixel = (-1 * s[x-3, y] + 9 * s[x-1, y] + 9 * s[x+1, y] - 1 * s[x+3, y]) / 16
-    veritcalPixel = (-1 * s[x, y-3] + 9 * s[x, y-1] + 9 * s[x, y+1] - 1 * s[x, y+3]) / 16
+    veritcalPixel   = (-1 * s[x, y-3] + 9 * s[x, y-1] + 9 * s[x, y+1] - 1 * s[x, y+3]) / 16
 
     op = horizontalPixel * weight1 + veritcalPixel * weight2
     op = 255 if op > 255 else op
